5edf8557d914998bac765409de71bb6bc30419f1
Bartek Mucha <muchzill4@gmail.com>
2020-04-21 13:05:32 +0100

    WIP

--- mbed_build/_internal/config/config.py
+++ mbed_build/_internal/config/config.py
@@ -45,21 +45,29 @@ def build_config_from_layers(layers: List[ConfigLayer]) -> Config:
     return config
 
 
+def _apply_layers(config: Config, layers: List[ConfigLayer]) -> Config:
+    # TODO: don't mutate
+    for layer in layers:
+        layer.apply(config)
+    return config
+
+
 def build_config(mbed_program_directory, board_type) -> Config:
     target = get_target_by_board_type(board_type, mbed_program_directory)
-    config = _build_config_from_target(target)
+    base_layer = ConfigLayer.from_target_data(target)
+    initial_config = _apply_layers(_empty_config(), [base_layer])
+
     all_mbed_lib_files = find_files("mbed_lib.json", mbed_program_directory)
 
     old_filters = []
-    filters = _build_filters(config)
+    filters = _build_filters(initial_config)
 
     while old_filters != filters:
-        target_specific_mbed_lib_files = filter_files(all_mbed_lib_files, filters)
+        config_specific_mbed_lib_files = filter_files(all_mbed_lib_files, filters)
         mbed_lib_sources = [ConfigSource.from_file(file) for file in target_specific_mbed_lib_files]
         mbed_lib_layers = [ConfigLayer.from_config_source(source, target.labels) for source in mbed_lib_sources]
 
-        for layer in mbed_lib_layers:
-            layer.apply(config)
+        config = _apply_layers(initial_config, mbed_lib_layers)
 
         old_filters = filters
         filters = _build_filters(config)
@@ -87,5 +95,3 @@ def _empty_config() -> Config:
         settings={},
         target={"components": set(), "device_has": set(), "extra_labels": set(), "features": set(), "macros": set()},
     )
-
-
