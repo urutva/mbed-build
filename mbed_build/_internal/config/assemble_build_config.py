37882b9a998feed3441c11346e604f50dc525a15
Rob Walton <rob.walton@arm.com>
2020-07-02 13:00:40 +0100

    Refactor assemble build config to take Target and MbedProgram objects

--- mbed_build/_internal/config/assemble_build_config.py
+++ mbed_build/_internal/config/assemble_build_config.py
@@ -6,14 +6,29 @@
 from pathlib import Path
 from typing import Iterable, Optional
 
+from mbed_targets import Target
 from mbed_build._internal.config.config import Config
+from mbed_build._internal.config.bootloader_overrides import BootloaderOverrides
 from mbed_build._internal.config.cumulative_data import CumulativeData
 from mbed_build._internal.config.source import Source
 from mbed_build._internal.find_files import LabelFilter, filter_files, find_files
 from mbed_project import MbedProgram
 
 
-def assemble_config(mbed_target: str, mbed_program_directory: Path) -> Config:
+def assemble_config(mbed_target: Target, mbed_program: MbedProgram) -> Config:
+    """Assemble Config for given target and program directory.
+
+    The structure and configuration of MbedOS requires us to do multiple passes over
+    configuration files, as each pass might affect which configuration files should be included
+    in the final configuration.
+    """
+    target_source = Source.from_target(mbed_target)
+    mbed_lib_files = find_files("mbed_lib.json", mbed_program.root_path)
+    mbed_app_file = mbed_program.files.app_config_file
+    return _assemble_config_from_sources_and_lib_files(target_source, mbed_lib_files, mbed_app_file)
+
+
+def assemble_bootloader_overrides(mbed_target: Target, mbed_program_directory: Path) -> Config:
     """Assemble Config for given target and program directory.
 
     The structure and configuration of MbedOS requires us to do multiple passes over
@@ -24,7 +39,7 @@ def assemble_config(mbed_target: str, mbed_program_directory: Path) -> Config:
     mbed_lib_files = find_files("mbed_lib.json", mbed_program_directory)
     mbed_program = MbedProgram.from_existing(mbed_program_directory)
     mbed_app_file = mbed_program.files.app_config_file
-    return _assemble_config_from_sources_and_lib_files(target_source, mbed_lib_files, mbed_app_file)
+    return _assemble_bootloader_overrides_from_sources_and_lib_files(target_source, mbed_lib_files, mbed_app_file)
 
 
 def _assemble_config_from_sources_and_lib_files(
@@ -34,10 +49,11 @@ def _assemble_config_from_sources_and_lib_files(
     current_cumulative_data = CumulativeData.from_sources([target_source])
     while True:
         filtered_files = _filter_files(mbed_lib_files, current_cumulative_data)
-        mbed_lib_sources = [Source.from_mbed_lib(file, current_cumulative_data.labels) for file in filtered_files]
+        mbed_lib_sources = [Source.from_mbed_lib(f, current_cumulative_data.labels) for f in filtered_files]
         all_sources = [target_source] + mbed_lib_sources
         if mbed_app_file:
             all_sources = all_sources + [Source.from_mbed_app(mbed_app_file, current_cumulative_data.labels)]
+
         previous_cumulative_data = current_cumulative_data
         current_cumulative_data = CumulativeData.from_sources(all_sources)
 
@@ -45,6 +61,24 @@ def _assemble_config_from_sources_and_lib_files(
             break
 
     return Config.from_sources(all_sources)
+
+
+def _assemble_bootloader_overrides_from_sources_and_lib_files(
+    target_source: Source, mbed_lib_files: Iterable[Path], mbed_app_file: Optional[Path] = None
+) -> Config:
+    previous_cumulative_data = None
+    current_cumulative_data = CumulativeData.from_sources([target_source])
+    while previous_cumulative_data != current_cumulative_data:
+        filtered_files = _filter_files(mbed_lib_files, current_cumulative_data)
+        mbed_lib_sources = [Source.from_mbed_lib(f, current_cumulative_data.labels) for f in filtered_files]
+        all_sources = [target_source] + mbed_lib_sources
+        if mbed_app_file:
+            all_sources = all_sources + [Source.from_mbed_app(mbed_app_file, current_cumulative_data.labels)]
+
+        previous_cumulative_data = current_cumulative_data
+        current_cumulative_data = CumulativeData.from_sources(all_sources)
+
+    return BootloaderOverrides.from_sources(all_sources)
 
 
 def _filter_files(files: Iterable[Path], cumulative_data: CumulativeData) -> Iterable[Path]:
