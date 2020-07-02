3499deb3ca7903dcce2b9aeda3b627d00e2ad60f
Rob Walton <rob.walton@arm.com>
2020-07-02 14:17:31 +0100

    Don't pass mbed-os profile dir to get_build_flags

--- mbed_build/_internal/context/create_build_context.py
+++ mbed_build/_internal/context/create_build_context.py
@@ -12,7 +12,7 @@ def create_build_context(toolchain_name, program_path, target_name):
     target = get_target_by_name(target_name, program.root_path)
     toolchain = Toolchain.from_name(toolchain_name)
     files = BuildFiles(program.root_path, target, toolchain.labels)
-    flags = get_build_flags(program.mbed_os.profiles_directory, toolchain, target)
+    flags = get_build_flags(toolchain, target)
 
     return BuildContext(
         project_name=program.root_path.stem,
